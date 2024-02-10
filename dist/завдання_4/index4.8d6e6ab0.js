51cd370f930e2908530ad0e1da93b7d735135000
diana7770 <20diane09@gmail.com>
2024-02-10 09:14:42 +0200

    gh

---
+++ dist/завдання_4/index4.8d6e6ab0.js
@@ -0,0 +1,2 @@
+const u=document.getElementById("time");function e(){""!==u.value&&alert(`\u{412}\u{438} \u{432}\u{432}\u{435}\u{43B}\u{438} ${u.value} \u{441}\u{435}\u{43A}\u{443}\u{43D}\u{434}`)}document.querySelector(".button").addEventListener("click",()=>{setTimeout(e,1e3*u.value)});
+//# sourceMappingURL=index4.8d6e6ab0.js.map
